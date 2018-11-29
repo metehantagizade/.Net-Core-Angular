a91e20bd9b8eb7d74bf6e11cd9bf592d412dd724
vahit.takirzade <vahit.takirzade@tiga.com.tr>
2018-11-29 11:43:30 +0300

    end of section 7

--- DatingApp/DatingApp-SPA/src/app/app-routing.module.ts
+++ DatingApp/DatingApp-SPA/src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MemberListComponent } from './member-list/member-list.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ListComponent } from './list/list.component';
+import { AuthGuard } from './_guards/auth.guard';
 
-const routes: Routes = [];
+// const routes: Routes = [
+//   {path: 'home', component: HomeComponent},
+//   {path: 'members', component: MemberListComponent, canActivate: [AuthGuard]},
+//   {path: 'messages', component: MessagesComponent, canActivate: [AuthGuard]},
+//   {path: 'lists', component: ListComponent, canActivate: [AuthGuard]},
+//   {path: '**', redirectTo: 'home', pathMatch: 'full'}
+// ];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard],
+    children: [
+      {path: 'members', component: MemberListComponent, canActivate: [AuthGuard]},
+      {path: 'messages', component: MessagesComponent, canActivate: [AuthGuard]},
+      {path: 'lists', component: ListComponent, canActivate: [AuthGuard]},
+    ]
+  },
+  {path: '**', redirectTo: '', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- DatingApp/DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp/DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -13,7 +14,8 @@ export class NavComponent implements OnInit {
   // used as public because we cannot use private authService on html page
   constructor(
     public authService: AuthService,
-    private alertify: AlertifyService
+    private alertify: AlertifyService,
+    private router: Router
   ) { }
 
   ngOnInit() {
@@ -24,6 +26,8 @@ export class NavComponent implements OnInit {
       this.alertify.success('Logged in successfullt');
     }, error => {
       this.alertify.error(error);
+    }, () => {
+      this.router.navigate(['/members']);
     });
   }
 
@@ -34,6 +38,7 @@ export class NavComponent implements OnInit {
   logout () {
     localStorage.removeItem('token');
     this.alertify.message('logged out');
+    this.router.navigate(['/home']);
   }
 
 }
