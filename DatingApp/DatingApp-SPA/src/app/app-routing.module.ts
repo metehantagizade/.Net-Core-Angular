4d80d3b571d95261fbc43b0eb03f6bc7f92fc29a
vahit.takirzade <vahit.takirzade@tiga.com.tr>
2018-12-06 10:48:19 +0300

    end of section 9

--- DatingApp/DatingApp-SPA/src/app/app-routing.module.ts
+++ DatingApp/DatingApp-SPA/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { ListComponent } from './list/list.component';
 import { AuthGuard } from './_guards/auth.guard';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
+import { MemberListResolver } from './_resolvers/member-list.resolver';
 
 // const routes: Routes = [
 //   {path: 'home', component: HomeComponent},
@@ -20,9 +23,10 @@ const routes: Routes = [
     runGuardsAndResolvers: 'always',
     canActivate: [AuthGuard],
     children: [
-      {path: 'members', component: MemberListComponent, canActivate: [AuthGuard]},
-      {path: 'messages', component: MessagesComponent, canActivate: [AuthGuard]},
-      {path: 'lists', component: ListComponent, canActivate: [AuthGuard]},
+      {path: 'members', component: MemberListComponent, resolve: {users: MemberListResolver}},
+      {path: 'members/:id', component: MemberDetailComponent, resolve: {user: MemberDetailResolver}},
+      {path: 'messages', component: MessagesComponent},
+      {path: 'lists', component: ListComponent},
     ]
   },
   {path: '**', redirectTo: '', pathMatch: 'full'}
